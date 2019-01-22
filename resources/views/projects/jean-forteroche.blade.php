b0213f3c018bd2100ec88264d68b78368c672002
Jean-MArc Voisin <jeanmarc.voisin.tai@gmail.com>
2019-01-22 10:43:12 +0100

    Mise en page de la présentation du projet forteroche

--- resources/views/projects/jean-forteroche.blade.php
+++ resources/views/projects/jean-forteroche.blade.php
@@ -1,1 +1,9 @@
 @extends('layouts.app')
+
+<p class="lead py-3">Première photo de la page d'accueil avec un texte genre : premier projet en PHP, langage très sympa pour moi, développement de ce projet avec beaucoup de plaisir.</p>
+
+<p class="lead py-3">Deuxième photo : blog avec une vue qui affiche un résumé des articles avec la date et une organisation antéchronologique.premie rprojet en PHP, langage très sympa pour moi, développement de ce projet avec beaucoup de plaisir. Vue gérer avec des card de bootstrap.</p>
+
+<p class="lead py-3">Troisième photo : vue générale (zoom faible) d'un article avec le bloc de commentaire correspondant en bas de page.</p>
+
+<p class="lead py-3">Quatrième photo : Vue du bloc de commentaire, ici avec un utilisateur non connecté. Nous voyons les commentaires postés avec leurs dates et les auteurs aisni que leurs avatars.</p>
